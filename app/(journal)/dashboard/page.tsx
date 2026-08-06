aace8742fe139167e6485438b0d326e76ea4dfd8
David Ang <davidang09@gmail.com>
2026-08-06 20:21:47 +0800

    Merge portfolio view into unified dashboard

--- app/(journal)/dashboard/page.tsx
+++ app/(journal)/dashboard/page.tsx
@@ -279,6 +279,9 @@ export default function DashboardPage() {
 
       <MonthlyCalendar summaries={summaries} />
 
+      {/* Open Positions & Manual Entry Card */}
+      <OpenPositionsCard onTradeAdded={() => setRefreshKey((k) => k + 1)} />
+
       <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
         <div className="lg:col-span-2">
           <CumulativePnLChart
